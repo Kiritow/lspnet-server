6f2f9e0013c1369450aafe61b78a4740b6c39cec
Kiritow <1362050620@qq.com>
2024-10-02 08:50:32 +0000

    migrate cred

---
+++ src/credentials.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+
+export function GetMySQLOptionSync() {
+    return JSON.parse(
+        fs.readFileSync("mysql.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function GetRedisOptionSync() {
+    return JSON.parse(
+        fs.readFileSync("redis.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function GetServiceTokenKeysSync() {
+    return JSON.parse(
+        fs.readFileSync("service_token.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function GetInfluxDBOptionSync() {
+    return JSON.parse(
+        fs.readFileSync("influxdb.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function GetGithubOAuthAppSync() {
+    return JSON.parse(
+        fs.readFileSync("oauth_github.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
+
+export function GetKoaAppSecretSync() {
+    return JSON.parse(
+        fs.readFileSync("koa.secret", {
+            encoding: "utf-8",
+        })
+    );
+}
